40a7932b1a1bc17da97026b736d58b5a84c8c0d7
Eugene <budinverano@gmail.com>
2023-10-07 02:23:35 +0800

    blackjack

---
+++ test.py
@@ -0,0 +1,14 @@
+l1 = ["eat", "sleep", "repeat"]
+
+# printing the tuples in object directly
+for ele in l1:
+    print(ele)
+
+# # changing index and printing separately
+# for count, ele in enumerate(l1, 100):
+#     print(count, ele)
+
+# # getting desired output from tuple
+# for ele, count in enumerate(l1):
+#     print(count)
+#     print(ele)
